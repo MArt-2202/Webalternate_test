b03baab00333c8719f683bcf535089597416478d
Artem <mart_22@ukr.net>
2022-03-29 19:20:56 +0300

    scripts.js: import/init blockPosition, blockStyles, toggleContent

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,14 +1,21 @@
 'use strict';
 import debounce from './modules/debounce';
 import footerToBottomPage from './modules/footerToBottomPage';
+import blockPosition from './modules/blockPosition';
+import blockStyles from './modules/blockStyles';
+import toggleContent from './modules/toggleContent';
 
 document.addEventListener('DOMContentLoaded', () => {
+	toggleContent();
+	blockStyles();
 	debounce(function () {
+		blockPosition();
 		footerToBottomPage();
 	}, 200);
 }); // END READY
 window.addEventListener('resize', () => {
 	debounce(function () {
+		blockPosition();
 		footerToBottomPage();
 	}, 200);
 });
