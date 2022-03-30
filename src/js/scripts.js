54a81f9823b051f285943e0b43df678377fd5098
Artem <mart_22@ukr.net>
2022-03-30 20:01:51 +0300

    scripts.js: import/init tabs

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -3,6 +3,7 @@ import debounce from './modules/debounce';
 import footerToBottomPage from './modules/footerToBottomPage';
 import blockPosition from './modules/blockPosition';
 import blockStyles from './modules/blockStyles';
+import tabs from './modules/tabs';
 import toggleContent from './modules/toggleContent';
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -11,12 +12,14 @@ document.addEventListener('DOMContentLoaded', () => {
 	debounce(function () {
 		blockPosition();
 		footerToBottomPage();
+		tabs({});
 	}, 200);
 }); // END READY
 window.addEventListener('resize', () => {
 	debounce(function () {
 		blockPosition();
 		footerToBottomPage();
+		tabs({});
 	}, 200);
 });
 
