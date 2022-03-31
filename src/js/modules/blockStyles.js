21ee800b08ccb26d5e88fa3e5045f44ffaeee004
Artem <mart_22@ukr.net>
2022-03-31 19:01:24 +0300

    blockStyles.js: Add calc width/height for items in steps project list

--- src/js/modules/blockStyles.js
+++ src/js/modules/blockStyles.js
@@ -1,7 +1,33 @@
 export default function blockStyles() {
 	if (document.querySelector('.header__nav a span')) {
 		document.querySelectorAll('.header__nav a span').forEach((el) => {
+			if (el.closest('a').hasAttribute('data-title')) {
+				return;
+			}
 			el.closest('a').setAttribute('data-title', el.textContent);
 		});
+	}
+
+	if (document.querySelector('.steps-list')) {
+		if (window.matchMedia('(min-width: 1025px)').matches) {
+			document.querySelectorAll('.steps-list__item').forEach((el) => {
+				const title = el.querySelector('strong'),
+					detail = el.querySelector('.steps-list__detail');
+
+				detail.style.width = title.clientWidth + 'px';
+				detail.style.height = title.clientHeight + 'px';
+			});
+		}
+	}
+
+	if (document.querySelector('.steps-list')) {
+		if (window.matchMedia('(max-width: 1024px)').matches) {
+			document.querySelectorAll('.steps-list__item').forEach((el) => {
+				const detail = el.querySelector('.steps-list__detail');
+
+				detail.style.width = '';
+				detail.style.height = '';
+			});
+		}
 	}
 }
